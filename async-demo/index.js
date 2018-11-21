ceda2f09d4a0896e09bcbef80585fbb9cdeae5b9
markt5660 <mark.thew@gmail.com>
2018-11-21 11:54:31 -0800

    Node.js Course, section 6, lecture 74

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,17 +1,26 @@
 console.log('Before');
 
 // Callback example
-getUser(1, (user) => {
-    console.log('User', user);
-    getRepositories(user.gitHubUsername, (repoList) => {
-        console.log('GitHub Repos', repoList);
-    });
-});
+getUser(1, fetchRepositories);
 
 console.log('After');
 
 // Promises
 // Async/Await
+
+function fetchRepositories (user) {
+    console.log('User', user);
+    getRepositories(user.gitHubUsername, fetchCommits);
+}
+
+function fetchCommits (repoList) {
+    console.log('Repos', repoList);
+    getCommits(repoList[0], displayCommits);
+}
+
+function displayCommits (commits) {
+    console.log('Commits', commits);
+}
 
 function getUser (id, callback) {
     // Simulated DB call that takes 2 seconds to complete
@@ -26,5 +35,13 @@ function getRepositories (gitHubUsername, callback) {
     setTimeout(() => {
         console.log('Fetching GitHub repos for user ', gitHubUsername, '...');
         callback(['repo1', 'repo2', 'repo3']);
+    }, 2000);
+}
+
+function getCommits (repo, callback) {
+    // Simulated GitHub call that takes 2 seconds to complete
+    setTimeout(() => {
+        console.log('Fetching GitHub commits for repo ', repo, '...');
+        callback(['commit1', 'commit2']);
     }, 2000);
 }
