ae048769fa229df4d605074f28e458547a9c9d6e
markt5660 <mark.thew@gmail.com>
2018-11-21 15:03:21 -0800

    Node.js Course, section 6, lecture 80

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,15 +1,30 @@
 console.log('Before');
 
 // Promises example
+/*
 getUser(1)
     .then(user => getRepositories(user.gitHubUsername))
     .then(repoList => getCommits(repoList[0]))
     .then(commitList => console.log('Commits', commitList))
     .catch(err => console.log('Error', err.message));
+*/
+
+// Async/Await w/ Promises
+async function displayCommits (userId) {
+    try {
+        const user = await getUser(userId);
+        const repoList = await getRepositories(user.gitHubUsername);
+        const commits = await getCommits(repoList[0]);
+        console.log(commits);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+displayCommits(1);
 
 console.log('After');
 
-// Async/Await
 
 function getUser (id) {
     return new Promise((resolve, reject) => {
@@ -26,7 +41,8 @@ function getRepositories (gitHubUsername) {
         // Simulated GitHub call that takes 2 seconds to complete
         setTimeout(() => {
             console.log('Fetching GitHub repos for user', gitHubUsername, '...');
-            resolve(['repo1', 'repo2', 'repo3']);
+            //resolve(['repo1', 'repo2', 'repo3']);
+            reject(new Error('getRepositories failed...'));
         }, 2000);
     });
 }
