06bfc329392f40083fa5fcfc982e9c20b4ee768f
markt5660 <mark.thew@gmail.com>
2018-11-24 12:09:56 -0800

    Node.js Course, section 7, lecture 90

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -37,8 +37,20 @@ async function createCourse () {
 }
 
 async function getCourses () {
+    // Comparison operators from MongoDB:
+    // eq (equal)
+    // ne (not equal)
+    // gt
+    // gte
+    // lt
+    // lte
+    // in
+    // nin (not in)
+
     const courses = await Course
-        .find({ author:'Mosh', isPublished: true })
+//        .find({ author:'Mosh', isPublished: true })
+//        .find({ price: { $gte: 10, $lte: 20 } })
+        .find({ price: { $in: [10, 15, 20] }})
         .limit(10)
         .sort({ name: 1 })
         .select({ name: 1, tags: 1});
