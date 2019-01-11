93700e734629a93d7f93a1a72849d4e248a829dd
Mark Thew <mark.thew@consolidated.com>
2019-01-11 14:58:53 -0800

    Node.js Course, section 11, lecture 158

--- vidly/startup/logging.js
+++ vidly/startup/logging.js
@@ -6,37 +6,39 @@ require('express-async-errors');
 module.exports = function () {
 
     // Setup global exception and promise rejection handlers
+/*
     process.on('uncaughtException', (ex) => {
         console.log('UNCAUGHT EXCEPTION');
         winston.error(ex.message, ex);
-        /* process.exit(1); // terminates but no logs generated */
+        process.exit(1); // terminates but no logs generated
     });
 
     process.on('unhandledRejection', (ex) => {
         console.log('UNHANDLED REJECTION');
         winston.error(ex.message, ex);
-        /* process.exit(1); // terminates but no logs generated */
+        process.exit(1); // terminates but no logs generated
     });
-
+*/
     /*
     ** Doesn't seem to work with this version of Winston
     ** Handlers catch exceptions and promise rejections
-    ** Process terminates but no logs generated
-
+    ** Process terminates but no logs generated (except console)
+    */
     winston.exceptions.handle(
-        new winston.transports.File({ filename: 'vidly.log' })
+        new winston.transports.File({ filename: 'vidly.log' }),
+        new winston.transports.Console({ colorize:true, prettyPrint:true })
     );
 
     process.on('unhandledRejection', (ex) => {
         throw ex;
     });
-    */
 
     // Setup logging
 
     winston.add(new winston.transports.File({ filename: 'vidly.log' }));
-
-    const mongoUrl = config.get('mongoUrl');
-    winston.add(new winston.transports.MongoDB({ db: mongoUrl }));
+    winston.add(new winston.transports.MongoDB({ 
+        db: config.get('mongoUrl'),
+        level: 'info'
+     }));
 
 }
