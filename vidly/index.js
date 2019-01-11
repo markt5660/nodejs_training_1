52eb8eeb8c59d30e34261ca051970ce6d149ae47
Mark Thew <mark.thew@consolidated.com>
2019-01-11 11:03:57 -0800

    Node.js Course, section 11, lecture 152

--- vidly/index.js
+++ vidly/index.js
@@ -36,15 +36,41 @@ if (!config.get('mongoUrl')) {
 
 const mongoUrl = config.get('mongoUrl');
 
-winston.add(new winston.transports.File({ filename: 'vidly.log' }));
-winston.add(new winston.transports.MongoDB({ db: mongoUrl }));
-
+// Setup global exception and promise rejection handlers
 process.on('uncaughtException', (ex) => {
     console.log('UNCAUGHT EXCEPTION');
     winston.error(ex.message, ex);
+    /* process.exit(1); // terminates but no logs generated */
 });
 
-throw new Error('Something failed during startup.');
+process.on('unhandledRejection', (ex) => {
+    console.log('UNHANDLED REJECTION');
+    winston.error(ex.message, ex);
+    /* process.exit(1); // terminates but no logs generated */
+});
+
+/*
+** Doesn't seem to work with this version of Winston
+** Handlers catch exceptions and promise rejections
+** Process terminates but no logs generated
+
+winston.exceptions.handle(
+    new winston.transports.File({ filename: 'vidly.log' })
+);
+
+process.on('unhandledRejection', (ex) => {
+    throw ex;
+});
+*/
+
+// Setup logging
+
+winston.add(new winston.transports.File({ filename: 'vidly.log' }));
+winston.add(new winston.transports.MongoDB({ db: mongoUrl }));
+
+// (TEMPORARY) Generate promise rejection to test handler
+const p = Promise.reject(new Error('Something failed miserably2'));
+p.then(() => console.log('Done'));
 
 // Connect to MongoDB (config controlled by env)
 mongoose.connect(mongoUrl)
