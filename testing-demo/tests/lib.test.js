0839d3e1ec799b0b849a3ec42aa05f8d52acda3a
markt5660 <mark.thew@gmail.com>
2019-01-26 10:03:27 -0800

    Node.js Course, section 12, lecture 166

--- testing-demo/tests/lib.test.js
+++ testing-demo/tests/lib.test.js
@@ -1,16 +1,20 @@
 const lib = require('../lib');
 
-test('absolute - positive return from postive number', () => {
-    const result = lib.absolute(1);
-    expect(result).toBe(1);
-});
+describe('absolute', () => {
 
-test('absolute - positive return from negative number', () => {
-    const result = lib.absolute(-1);
-    expect(result).toBe(1);
-});
-
-test('absolute - zero return from zero', () => {
-    const result = lib.absolute(0);
-    expect(result).toBe(0);
+    it('returns positive from postive input', () => {
+        const result = lib.absolute(1);
+        expect(result).toBe(1);
+    });
+    
+    it('returns positive from negative input', () => {
+        const result = lib.absolute(-1);
+        expect(result).toBe(1);
+    });
+    
+    it('returns zero from zero input', () => {
+        const result = lib.absolute(0);
+        expect(result).toBe(0);
+    });
+    
 });
